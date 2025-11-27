2e6f7bc60008d6eeaf9a74ad44009ee58cb569f5
DjanoVanEijk <9028315@student.zadkine.nl>
2025-11-27 13:48:53 +0100

    opdracht 5

---
+++ opdracht_5/script.js
@@ -0,0 +1,5 @@
+let string = "20";
+let number = 10;
+let x = string;
+let y = number;
+document.getElementById("lol").innerHTML = "als je deze string en getal bij elkaar op probeert te tellen krijg je: " + (x + y)
