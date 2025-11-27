950a24e526d720326311434873e16cfe880eb6fb
DjanoVanEijk <9028315@student.zadkine.nl>
2025-11-27 13:26:01 +0100

    wawawawaw

---
+++ opdracht_3/script.js
@@ -0,0 +1,5 @@
+
+//PLEASE JAVA I NEED THIS..
+let naam = prompt("Wat is je naam?");
+document.getElementById("tekst").innerHTML = "Hallo " + naam + ", welkom bij mijn eerste JavaScript-pagina!";
+console.log(naam)
