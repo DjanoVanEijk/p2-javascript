c7481ff747d21108a71f99f5ed40d1290a69034d
DjanoVanEijk <9028315@student.zadkine.nl>
2025-11-28 13:24:26 +0100

    opdracht 9

---
+++ Opdracht_9/script.js
@@ -0,0 +1,8 @@
+function verberg_tekst() {
+document.getElementById("id").style.display = "none"
+}
+
+function toon_tekst() {
+document.getElementById("id").style.display = "block"
+document.getElementById("id").innerHTML = "Peekaboo!"
+}
