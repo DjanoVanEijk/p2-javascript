516c8e6279a6d7278192826d5b118e0c7be574a5
DjanoVanEijk <9028315@student.zadkine.nl>
2025-11-28 13:52:13 +0100

    Hoofdopdracht 1

---
+++ hoofdopdracht_1/script.js
@@ -0,0 +1,23 @@
+function light_mode() {
+document.getElementById("id").innerHTML = "Good morning"
+document.body.style.backgroundColor = "white"
+document.body.style.color = "black"
+document.getElementById("button").style.backgroundColor = "white"
+document.getElementById("button").style.borderColor = "black"
+document.getElementById("button").style.color = "black"
+document.getElementById("button2").style.backgroundColor = "white"
+document.getElementById("button2").style.borderColor = "black"
+document.getElementById("button2").style.color = "black"
+}
+
+function dark_mode() {
+document.getElementById("id").innerHTML = "Good night"
+document.body.style.backgroundColor = "black"
+document.body.style.color = "white"
+document.getElementById("button").style.backgroundColor = "black"
+document.getElementById("button").style.borderColor = "white"
+document.getElementById("button").style.color = "white"
+document.getElementById("button2").style.backgroundColor = "black"
+document.getElementById("button2").style.borderColor = "white"
+document.getElementById("button2").style.color = "white"
+}
