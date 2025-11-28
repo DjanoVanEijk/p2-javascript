05872b2e1c8c2337d198fd7a1921848f207cb2c5
DjanoVanEijk <9028315@student.zadkine.nl>
2025-11-28 13:36:41 +0100

    opdracht 10

---
+++ opdracht_10/script.js
@@ -0,0 +1,17 @@
+function groet_ochtend() {
+document.getElementById("id").innerHTML = "Goedemorgen"
+document.body.style.backgroundColor = "yellow"
+document.body.style.color = "black"
+}
+
+function groet_middag() {
+document.getElementById("id").innerHTML = "Goedemiddag"
+document.body.style.backgroundColor = "orange"
+document.body.style.color = "black"
+}
+
+function groet_avond() {
+document.getElementById("id").innerHTML = "Goedeavond"
+document.body.style.backgroundColor = "darkblue"
+document.body.style.color = "white"
+}
