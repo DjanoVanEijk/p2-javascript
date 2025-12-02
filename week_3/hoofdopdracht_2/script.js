1c8402b4809c41c6fdf55ffae095ea976e1c5d59
DjanoVanEijk <9028315@student.zadkine.nl>
2025-12-02 14:46:12 +0100

    hoofdopdracht 2

--- week_3/hoofdopdracht_2/script.js
+++ week_3/hoofdopdracht_2/script.js
@@ -3,5 +3,28 @@ let multiplier = 1;
 
 function addpizza(){
     counter = counter + multiplier;
+    if (counter < 0){counter = counter + 1;}
+}
+
+
+function update(){
+document.getElementById("pizzacounter").innerHTML = counter + " Pizzas"
+
+
+if (counter >= 5){
+    document.getElementById("text").innerHTML = "Dat begint ergens op te lijken 😋"
+}
+
+if (counter >= 10){
+    document.getElementById("pizzamenu").style.display = "none"
+    document.getElementById("retrynow").style.display = "flex"
+}
+}
+
+function retry(){
+    document.getElementById("pizzamenu").style.display = "flex"
+    document.getElementById("retrynow").style.display = "none"
+    counter = 0
     document.getElementById("pizzacounter").innerHTML = counter + " Pizzas"
+    document.getElementById("text").innerHTML = "Welkom bij pizza clicker!"
 }
