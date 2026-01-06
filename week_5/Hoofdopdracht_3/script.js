a94b20e79e658d39f23540d908fd2826a10afb3c
DjanoVanEijk <9028315@student.zadkine.nl>
2026-01-06 14:03:25 +0100

    hoofdopdracht 3

---
+++ week_5/Hoofdopdracht_3/script.js
@@ -0,0 +1,18 @@
+let fortune = ["Ja", "Nee", "Nooit", "Zoiezo", "Nah", "Yup", "Geen kans", "100%", "Mischien" ,"Weet ik veel"];
+
+let input = document.getElementById("inputid");
+
+let button = document.getElementById("buttonid");
+
+button.addEventListener("click", iets);
+
+function iets(){
+if (input.value === ""){
+document.getElementById("id").innerHTML = "Vul iets in"
+}
+else{
+let randomIndex = Math.floor(Math.random() * fortune.length);
+let randomFortune = fortune[randomIndex];
+document.getElementById("id").innerHTML = randomFortune
+}}
+    
