68ca5cfe27c7e225b4e516e69e5f607223db2eb2
DjanoVanEijk <9028315@student.zadkine.nl>
2026-01-09 10:36:11 +0100

    kleine update

---
+++ week_6/opdracht_25/script.js
@@ -0,0 +1,18 @@
+let loadingbar = ["Loading. 10% 🐢", "Loading.. 20% 🐢", "Loading... 30% 🐢", "Loading. 40% 🏃‍♂️", "Loading.. 50% 🏃‍♂️", "Loading... 60% 🏃‍♂️", "Loading. 70% 🥇", "Loading.. 80% 🥇", "Loading... 90% 🥇"];
+
+let teller = 0;
+
+let timer;
+
+timer = setInterval(test, 1500)
+
+function test(){
+    if(teller === 8){
+        clearInterval(timer);
+        document.getElementById("id").innerHTML = "Loading complete!"
+    }
+    else{
+    teller = teller + 1
+    document.getElementById("id").innerHTML = loadingbar[teller]
+    }
+}
